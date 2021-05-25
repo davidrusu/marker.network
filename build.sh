bfcd96ea8131a1a5ad637f3b4f8749c4d05a7daa
Farhana Zameena Abbas <farhana.z.abbas@gmail.com>
2021-05-24 20:05:49 -0400

    get marker.network building on windows

--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cargo build --target x86_64-unknown-linux-musl --release --manifest-path marker-network-site-generator/Cargo.toml
 
     cp ./marker-network-site-generator/target/x86_64-unknown-linux-musl/release/marker_network_site_generator ./dist/marker_network_site_generator
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    echo "Building site generator for $OSTYPE"
+    cargo build --release --manifest-path marker-network-site-generator/Cargo.toml
+    cp ./marker-network-site-generator/target/release/marker_network_site_generator.exe ./dist/marker_network_site_generator.exe
 
 else
     echo "Building site generator for $OSTYPE"
